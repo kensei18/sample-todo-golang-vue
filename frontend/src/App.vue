2ac46876c9e92df5745833ec5263f805e27550a7
Kensei Sakai <kensei.sakai@gmail.com>
2021-04-25 13:03:16 +0900

    implement frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,159 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="4">
+          <h1>ToDo</h1>
+          <v-btn class="mb-2" color="primary" @click="showNewDialog"
+            >Create</v-btn
+          >
+          <TaskList :task-list="todoList" @update-status="updateStatus(0)" />
+        </v-col>
+
+        <v-divider vertical></v-divider>
+
+        <v-col cols="4">
+          <h1>Doing</h1>
+          <TaskList :task-list="doingList" @update-status="updateStatus(1)" />
+        </v-col>
+
+        <v-divider vertical></v-divider>
+
+        <v-col cols="4">
+          <h1>Done</h1>
+          <TaskList :task-list="doneList" @update-status="updateStatus(2)" />
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title>{{ dialogHeader }}</v-card-title>
+        <v-card-text>
+          <v-text-field v-model="name" label="Task Name"></v-text-field>
+          <v-textarea
+            v-model="description"
+            label="Task Description"
+            outlined
+          ></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="green" dark @click="savingAction">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
 </template>
 
 <script>
+import TaskList from "./TaskList";
+
 export default {
-  name: "App",
+  components: {
+    TaskList,
+  },
+
+  provide() {
+    return {
+      updateSelectedTaskId: this.updateSelectedTaskId,
+      showEditDialog: this.showEditDialog,
+      removeTask: this.removeTask,
+    };
+  },
 
   data: () => ({
-    //
+    tasks: [
+      { id: 1, name: "todo1", description: "hogehoge", status: 0 },
+      { id: 2, name: "todo2", description: "hogehoge", status: 0 },
+      { id: 3, name: "doing1", description: "hogehoge", status: 1 },
+      { id: 4, name: "doing2", description: "hogehoge", status: 1 },
+      { id: 5, name: "done1", description: "hogehoge", status: 2 },
+      { id: 6, name: "done2", description: "hogehoge", status: 2 },
+    ],
+    dialog: false,
+    selectedTaskId: 0,
+    dialogHeader: "Create Task",
+    savingAction: null,
+    name: "",
+    description: "",
   }),
+
+  computed: {
+    todoList() {
+      return this.taskList(0);
+    },
+    doingList() {
+      return this.taskList(1);
+    },
+    doneList() {
+      return this.taskList(2);
+    },
+  },
+
+  watch: {
+    dialog(value) {
+      if (!value) {
+        this.name = "";
+        this.description = "";
+        this.selectedTaskId = 0;
+      }
+    },
+  },
+
+  methods: {
+    taskList(status) {
+      return this.tasks.filter((task) => task.status === status);
+    },
+    findTaskById(id) {
+      return this.tasks.find((task) => task.id === id);
+    },
+    updateSelectedTaskId(id) {
+      this.selectedTaskId = id;
+    },
+    updateStatus(status) {
+      const task = this.findTaskById(this.selectedTaskId);
+      task.status = status;
+      this.selectedTaskId = 0;
+    },
+    showNewDialog() {
+      this.dialog = true;
+      this.selectedTaskId = 0;
+      this.dialogHeader = "Create Task";
+      this.savingAction = this.createTask;
+    },
+    createTask() {
+      // use API
+      const maxId = this.tasks.reduce((acc, cur) =>
+        acc.id > cur.id ? acc : cur
+      ).id;
+      this.tasks.push({
+        id: maxId + 1,
+        name: this.name,
+        description: this.description,
+        status: 0,
+      });
+      this.dialog = false;
+    },
+    showEditDialog(id) {
+      const task = this.findTaskById(id);
+      this.dialog = true;
+      this.dialogHeader = "Edit Task";
+      this.name = task.name;
+      this.description = task.description;
+      this.selectedTaskId = id;
+      this.savingAction = this.updateTask;
+    },
+    updateTask() {
+      const task = this.findTaskById(this.selectedTaskId);
+      this.$set(task, "name", this.name);
+      this.$set(task, "description", this.description);
+      this.dialog = false;
+    },
+    removeTask(id) {
+      const idx = this.tasks.findIndex((task) => task.id === id);
+      this.tasks.splice(idx, 1);
+    },
+  },
 };
 </script>
---
+++ frontend/src/TaskList.vue
@@ -0,0 +1,41 @@
+<template>
+  <draggable
+    group="task-list"
+    style="min-height: 90vh"
+    @add="$emit('update-status')"
+  >
+    <v-card
+      v-for="task in taskList"
+      :key="task.id"
+      class="my-2"
+      color="cyan lighten-5"
+      @dragstart="updateSelectedTaskId(task.id)"
+    >
+      <v-card-title>
+        <span>{{ task.name }}</span>
+        <v-spacer />
+        <v-btn fab text icon x-small @click="showEditDialog(task.id)">
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-btn fab text icon x-small @click="removeTask(task.id)">
+          <v-icon>mdi-delete</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text>{{ task.description }}</v-card-text>
+    </v-card>
+  </draggable>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: { draggable },
+
+  props: {
+    taskList: Array,
+  },
+
+  inject: ["updateSelectedTaskId", "showEditDialog", "removeTask"],
+};
+</script>
